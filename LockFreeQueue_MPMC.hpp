b6ccf7b652554030e999e9b44c232b10e320f19d
MD AZAD ALI SHAH <46038479+sheikhazad@users.noreply.github.com>
2026-05-31 04:05:02 +0100

    Remove unnecessary constants from LockFreeQueue_MPMC.hpp
    Removed constants for producers, consumers, workload, and NUMA nodes.

--- LockFreeQueue_MPMC.hpp
+++ LockFreeQueue_MPMC.hpp
@@ -13,11 +13,6 @@
 #define hardware_destructive_interference_size 64
 #endif
 
-constexpr int NUM_PRODUCERS = 4;
-constexpr int NUM_CONSUMERS = 4;
-constexpr int WORKLOAD = 1000;
-constexpr int NUMA_NODE_0 = 0;
-constexpr int NUMA_NODE_1 = 1;
 constexpr size_t CACHE_LINE_SIZE = hardware_destructive_interference_size;
 
 
